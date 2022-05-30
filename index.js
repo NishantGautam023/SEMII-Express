d233f633b474c8d6d8bf804fd7d9b183c1a9d33a
Nishant Gautam <menishantgautam@gmail.com>
2022-05-30 19:58:09 -0400

    EJS template engine

--- index.js
+++ index.js
@@ -1,26 +1,40 @@
 // console.log("Assignment 1");
 
 const express = require("express");
-const format = require("date-format")
+// const format = require("date-format")
 const app = express();
+app.set("view engine", "ejs")
 // app.use(express.static("public"))
 const PORT = process.env.PORT || 4000;
-const path = require("path")
+// const path = require("path")ls
+
+
+const user = {
+    firstName: 'Nishant',
+    lastName: 'Gautam',
+}
 
 
 // Home Page 
 app.get("/", (req,res) => {
 
     // res.sendFile(path.resolve(__dirname,'public/htmls/index.html'))
-    res.sendFile('htmls/index.html', {root: 'public'});
+    // res.sendFile('htmls/index.html', {root: 'public'});
 
     // res.status(200).send("<h1>Welcome to the Home Page</h1>")
+
+    res.render('pages/index', {
+        user: user
+    })
+
+
 })
 
 
 // G route
 app.get("/G", (req,res) => {
     res.status(200).send(`<h1>Get Ready for your G exam ${format.asString("dd:MM:yyyy", new Date())}  </h1>`)
+   
 })
 
 
