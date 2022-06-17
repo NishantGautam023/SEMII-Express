212c40ded04ef987a26d558cf27b31da70cee2fc
Nishant Gautam <menishantgautam@gmail.com>
2022-06-17 08:55:22 -0400

    Schema Definition

---
+++ models/userDetails.js
@@ -0,0 +1,21 @@
+const mongoose = require("mongoose") // Require Mongoose
+const {schema} = mongoose;   
+
+// Schema Definition 
+
+const userDetailsSchema = new Schema({
+    firstName: String,
+    lastName: String,
+    License_No: String,
+    Age: Number,
+    car_details: {
+        make: String,
+        model: String,
+        year: Number,
+        platNo: String
+    },
+    
+});
+
+const userModel = mongoose.model("userModel", userDetailsSchema);
+module.exports = userModel;
